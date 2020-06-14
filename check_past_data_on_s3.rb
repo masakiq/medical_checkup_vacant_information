32771dd5a213c107edd3ae9fc57f2243f33e08bf
maeda1150 <>
2020-06-14 14:33:03 +0900

    Add some logic

--- check_past_data_on_s3.rb
+++ check_past_data_on_s3.rb
@@ -3,9 +3,37 @@
 require 'aws-sdk-s3'
 # check_past_data_on_s3.rb
 class CheckPastDataOnS3
+  def initialize
+    @client = Aws::S3::Client.new
+  end
+
   def call
-    s3 = Aws::S3::Client.new
-    resp = s3.list_buckets
+    hogehoge('hoge')
+  end
+
+  private
+
+  attr_reader :client
+
+  def fuga
+    resp = client.list_buckets
     resp.buckets.map(&:name)
+  end
+
+  def hoge
+    resp = client.list_objects({
+      bucket: 'logs-portfolio',
+      max_keys: 2,
+    })
+  end
+
+  def hogehoge(key)
+    resp = client.get_object({
+      bucket: 'medical-checkup-vacant-information',
+      key: key,
+      range: 'bytes=0-9',
+    })
+  rescue Aws::S3::Errors::NoSuchKey => e
+    puts 'no such key'
   end
 end
