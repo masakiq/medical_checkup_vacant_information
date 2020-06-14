c779effe1f8cc8eb10c6070f99a27c101bdb609b
maeda1150 <>
2020-06-14 17:50:02 +0900

    zip.sh -> deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+cp -f *.rb tmp/
+rm tmp/main.rb
+chmod -R 755 tmp/*
+rm dest/medicalCheckupVacantInformation.zip
+cd tmp && zip -r ../dest/medicalCheckupVacantInformation.zip . && cd -
+rm tmp/*.rb
+
+aws lambda update-function-code \
+  --function-name medical_checkup_vacant_information \
+  --zip-file fileb://`pwd`/dest/medicalCheckupVacantInformation.zip
+
+rm dest/medicalCheckupVacantInformation.zip
