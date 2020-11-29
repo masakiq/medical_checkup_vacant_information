7c819cc8c866b8e2b216e0ebf9983c46eb87cd5c
maeda1150 <>
2020-11-29 17:29:40 +0900

    Refactoring
    modified:   lambda_function.rb

--- lambda_function.rb
+++ lambda_function.rb
@@ -9,17 +9,31 @@ unless ENV['development']
 end
 
 def lambda_handler(event:, context:) # rubocop:disable Lint/UnusedMethodArgument,Metrics/AbcSize,Metrics/MethodLength
+  # get current vacants
   html_elements = FetchHtml.execute
   current_vacants = ScrapingVacant.execute(html_elements)
+
+  # get past vacants
   past_vacants = VacantRepository.new.find_all
+
+  # merge past_number to current vacants
   merged_vacants = MergePastNumberToCurrentVacant.new.execute(current_vacants, past_vacants)
-  filter_increased_vacants = FilterIncreasedVacant.new.execute(merged_vacants)
-  filter_become_to_zero_vacants = FilterBecomeToZeroVacant.new.execute(merged_vacants)
-  increased_vacant_payload = IncreasedVacantPayloadBuilder.new.execute(filter_increased_vacants)
-  become_to_zero_vacant_payload = BecomeToZeroVacantPayloadBuilder.new.execute(filter_become_to_zero_vacants)
+
+  # filter vacants
+  increased_vacants = FilterIncreasedVacant.new.execute(merged_vacants)
+  become_to_zero_vacants = FilterBecomeToZeroVacant.new.execute(merged_vacants)
+
+  # build payload
+  increased_vacant_payload = IncreasedVacantPayloadBuilder.new.execute(increased_vacants)
+  become_to_zero_vacant_payload = BecomeToZeroVacantPayloadBuilder.new.execute(become_to_zero_vacants)
+
+  # notify to slack
   slack_notifier = NotifySlack.new
   slack_notifier.execute(payload: increased_vacant_payload, icon_emoji: 'zou')
   slack_notifier.execute(payload: become_to_zero_vacant_payload, icon_emoji: 'gen')
+
+  # persist vacants
   PersistVacant.new.execute(current_vacants)
+
   { statusCode: 200, body: JSON.generate('Hello from Lambda!') }
 end
