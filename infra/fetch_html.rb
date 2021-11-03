1587a6493b63abb8434bbf6449f0c786b4239370
masakiq <>
2021-11-03 21:52:49 +0900

    Make KENPO_URL an environment variable
    new file:   .envrc
    modified:   infra/fetch_html.rb

--- infra/fetch_html.rb
+++ infra/fetch_html.rb
@@ -4,12 +4,10 @@
 class FetchHtml < AbstractFetchHtml
   require 'open-uri'
 
-  KENPO_URL = 'https://ks.its-kenpo.or.jp/customer/vacancies'
-
   # @param url [String] Target url
   # @return [Array<String>] description
   def self.execute
-    content = OpenURI.open_uri(KENPO_URL).read
+    content = OpenURI.open_uri(ENV['KENPO_URL']).read
     content.split("\n").map(&:strip)
   end
 end
